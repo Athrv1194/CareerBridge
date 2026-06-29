604bd02b04d75b18addeae441a20139c83b3faa9
Smaddy98 <smadge98@gmail.com>
2026-06-29 21:22:45 +0530

    chore(git): remove tracked build artifacts and commit only source files

--- CareerBridgeSolution/CareerBridge.API/Program.cs
+++ CareerBridgeSolution/CareerBridge.API/Program.cs
@@ -3,7 +3,6 @@ using CareerBridge.API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace CareerBridge.API
@@ -27,37 +26,7 @@ namespace CareerBridge.API
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CareerBridge API", Version = "v1" });
-                
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header
-                        },
-                        new List<string>()
-                    }
-                });
-            });
+            builder.Services.AddSwaggerGen();
 
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             builder.Services.AddDbContext<AppDbContext>(options =>
@@ -75,14 +44,6 @@ namespace CareerBridge.API
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
                     };
                 });
-
-            builder.Services.AddCors(options => {
-                options.AddPolicy("AllowReactApp", policy => {
-                    policy.WithOrigins("http://localhost:5173") // React Dev Server Port
-                          .AllowAnyHeader()
-                          .AllowAnyMethod();
-                });
-            });
 
             var app = builder.Build();
 
@@ -110,8 +71,6 @@ namespace CareerBridge.API
             }
 
             app.UseHttpsRedirection();
-
-            app.UseCors("AllowReactApp");
 
             app.UseAuthentication();
             app.UseAuthorization();
